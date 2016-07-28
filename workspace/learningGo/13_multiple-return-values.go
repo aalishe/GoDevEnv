7ab46c7bd8933141cc266a2469098b82b65b561b
Johandry Amador <johandry@yahoo.com>
2016-07-27 21:26:10 -0700

    Exercise #13

--- workspace/learningGo/13_multiple-return-values.go
+++ workspace/learningGo/13_multiple-return-values.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
-func main() {
-
+func vals() (int, int)  {
+  return 3, 7
 }
 
+func main() {
+  a, b := vals()
+  fmt.Println(a)
+  fmt.Println(b)
+
+  _, c := vals()
+  fmt.Println(c)  
+}
