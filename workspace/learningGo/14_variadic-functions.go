4f390618dad5c1ae401e14ea066f651d004f4063
Johandry Amador <johandry@yahoo.com>
2016-07-27 21:40:52 -0700

    Exercise #14

--- workspace/learningGo/14_variadic-functions.go
+++ workspace/learningGo/14_variadic-functions.go
@@ -2,7 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-
+func sum(nums ...int)  {
+  fmt.Print(nums, " ")
+  total := 0
+  for _, num := range nums {
+    total += num
+  }
+  fmt.Println(total)
 }
+func main() {
+  sum(1,2)
+  sum(1,2,3)
 
+  nums := []int{1, 2, 3, 4, 5}
+  sum(nums...)
+}
