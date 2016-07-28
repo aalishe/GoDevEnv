bc3b24a2764f873ceda493f595c2f6885230da84
Johandry Amador <johandry@yahoo.com>
2016-07-27 21:53:33 -0700

    Exercise #16

--- workspace/learningGo/16_recursion.go
+++ workspace/learningGo/16_recursion.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-
+func fact(n int) int  {
+  if n == 0 {
+    return 1
+  }
+  return n * fact(n-1)
 }
 
+func main() {
+  fmt.Println("7! = ",fact(7))
+}
