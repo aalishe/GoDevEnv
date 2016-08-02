bcdcb5a83641893bc75c11be0fa3ce01fa48b5e7
Johandry Amador <johandry@yahoo.com>
2016-08-01 22:48:45 -0700

    Example 32,33: Timers and Tickers

--- workspace/learningGo/33_tickers.go
+++ workspace/learningGo/33_tickers.go
@@ -3,7 +3,17 @@ package main
 /* Example #33: https://gobyexample.com/tickers */
 
 import "fmt"
+import "time"
 
 func main() {
+  ticker := time.NewTicker(time.Millisecond * 500)
+  go func ()  {
+    for t := range ticker.C {
+      fmt.Println("Tick at", t)
+    }
+  }()
 
+  time.Sleep(time.Millisecond * 2600)
+  ticker.Stop()
+  fmt.Println("Ticker stopped")
 }
