46d020969844d0bb605149d7a69735f7e64c5e55
Johandry Amador <johandry@yahoo.com>
2016-08-01 22:26:59 -0700

    Examples 27-31: More channels

--- workspace/learningGo/31_range-over-channels.go
+++ workspace/learningGo/31_range-over-channels.go
@@ -5,5 +5,12 @@ package main
 import "fmt"
 
 func main() {
+  queue := make(chan string, 2)
+  queue <- "one"
+  queue <- "two"
+  close(queue)
 
+  for elem := range queue {
+    fmt.Println(elem)
+  }
 }
