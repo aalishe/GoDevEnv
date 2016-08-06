c21fdeb9b87ddfaf6e1368bbd3d872cbe19caf40
Johandry Amador <johandry@yahoo.com>
2016-08-06 14:10:48 -0700

    Example #36

--- workspace/learningGo/36_atomic-counters.go
+++ workspace/learningGo/36_atomic-counters.go
@@ -3,7 +3,24 @@ package main
 /* Example #36: https://gobyexample.com/atomic-counters */
 
 import "fmt"
+import "time"
+import "sync/atomic"
+import "runtime"
 
 func main() {
+  var ops uint64 = 0
 
+  for i := 0; i < 50; i++ {
+    go func()  {
+      for {
+        atomic.AddUint64(&ops, 1)
+        runtime.Gosched()
+      }
+    }()
+  }
+
+  time.Sleep(time.Second)
+
+  opsFinal := atomic.LoadUint64(&ops)
+  fmt.Println("Ops: ", opsFinal)
 }
