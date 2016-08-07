0d770db588b586c72d8c0d1e9ccd073197d7526a
Johandry Amador <johandry@yahoo.com>
2016-08-07 09:59:59 -0700

    Examples #51 - 54

--- workspace/learningGo/54_sha1-hashes.go
+++ workspace/learningGo/54_sha1-hashes.go
@@ -3,7 +3,22 @@ package main
 /* Example #54: https://gobyexample.com/sha1-hashes */
 
 import "fmt"
+import "crypto/sha1"
+import "crypto/md5"
 
 func main() {
+  s := "sha1 this string"
+  h := sha1.New()
+  h.Write([]byte(s))
+  bs := h.Sum(nil)
 
+  fmt.Println(s)
+  fmt.Printf("SHA1: %x\n", bs)
+
+  m := md5.New()
+  m.Write([]byte(s))
+  bms := m.Sum(nil)
+
+  fmt.Println(s)
+  fmt.Printf("MD5: %x\n", bms)
 }
