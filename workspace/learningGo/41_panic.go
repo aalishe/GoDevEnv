9cf716de47a5d5462f8719d62e07deb951dc07d3
Johandry Amador <johandry@yahoo.com>
2016-08-06 18:55:36 -0700

    Examples #41 - #45

--- workspace/learningGo/41_panic.go
+++ workspace/learningGo/41_panic.go
@@ -2,8 +2,13 @@ package main
 
 /* Example #41: https://gobyexample.com/panic */
 
-import "fmt"
+import "os"
 
 func main() {
+  panic("a problem")
 
+  _, err := os.Create("/tmp/file")
+  if err != nil {
+    panic(err)
+  }
 }
--- workspace/learningGo/42_defer.go
+++ workspace/learningGo/42_defer.go
@@ -3,7 +3,29 @@ package main
 /* Example #42: https://gobyexample.com/defer */
 
 import "fmt"
+import "os"
 
 func main() {
+  f := createFile("/tmp/defer.txt")
+  defer closeFile(f)
+  writeFile(f)
+}
 
+func createFile(p string) *os.File {
+  fmt.Println("Creating")
+  f, err := os.Create(p)
+  if err != nil {
+    panic(err)
+  }
+  return f
+}
+
+func writeFile( f *os.File)  {
+  fmt.Println("Writing")
+  fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File)  {
+  fmt.Println("Closing")
+  f.Close()
 }
